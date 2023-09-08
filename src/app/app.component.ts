6bcf53b6f1348c44521c11e5834badb67a5f3e1d
AdityaDhiman1 <adityadhiman251001mzn@gmail.com>
2023-09-08 13:29:47 +0530

    read me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { faX,faBars } from '@fortawesome/free-solid-svg-icons';
+import * as $ from  'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularrouting';
+  faX = faX;
+  faBars = faBars;
+  constructor() { }
+  ngOnInit() {
+    // $('.open').on('click', function () {
+    //   $('ul').show();
+    //   $(this).hide();
+    //   $('.close').show();
+    //   $('.close').on('click', function () {
+    //     $(this).hide();
+    //     $('ul').hide();
+    //     $('.open').show();
+    //    })
+    // });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
